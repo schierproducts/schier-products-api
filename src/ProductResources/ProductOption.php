bc470c1d32e4a1b39ff11449efde1786177e4b9e
Doug Niccum <doug.niccum@schierproducts.com>
2022-05-27 09:14:23 -0500

    Changes the property type of the product option price.

--- src/ProductResources/ProductOption.php
+++ src/ProductResources/ProductOption.php
@@ -12,7 +12,7 @@ namespace SchierProducts\SchierProductApi\ProductResources;
  * @property string $name The people-friendly name of the option
  * @property string $option_type The type of option that is being represented: cover, connection, etc
  * @property string|null $description Any other contextually relevant information associated with this option
- * @property string $price The markup of the option
+ * @property ProductPrice $price The markup and pricing of the option
  * @property string $store_id The id of the option within the eCommerce platform
  */
 class ProductOption extends ProductResource
